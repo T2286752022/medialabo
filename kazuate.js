7911bd1e75e54acd8ecdcda7e30a412a04a18f50
T2286752022 <228675@st.takushoku-u.ac.jp>
2023-07-06 15:00:21 +0900

    課題

--- kazuate.js
+++ kazuate.js
@@ -3,20 +3,55 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
-
+let kaisu = 1;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click', hantei);
+
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let i = document.querySelector('input[name="yoso"]');
+  let yoso=i.value;
+  
+  kaisu+=1;
+  let k1 = document.querySelector('span#kaisu');
+  k1.textContent = kaisu; 
+  let k2 = document.querySelector('span#answer');
+  k2.textContent = yoso;
+  console.log(kaisu+'回目の予想:' +yoso);
+  let p1 = document.querySelector('p#result');
+
+  if (kaisu >= 4) {
+    p1.textContent = ("答えは" + kotae +"でした．すでにゲームは終わっています");
+    console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
+  }else if(kaisu==3 && kotae!=yoso){
+    p1.textContent = ("まちがい.残念でした答えは" + kotae +"です．"); 
+    console.log("まちがい.残念でした答えは" + kotae +"です．");
+  }else if(kaisu==3 && kotae==yoso ){
+    p1.textContent = ("正解です.おめでとう!");
+    console.log("正解です.おめでとう!");
+  
+
+  }
+  else{
+    if (yoso > kotae) {
+      p1.textContent = ("まちがい.答えはもっと小さいですよ");
+        console.log("まちがい.答えはもっと小さいですよ");
+      }
+    
+    else if(yoso < kotae) {
+      p1.textContent = ("まちがい.答えはもっと大きいですよ");
+        console.log("まちがい.答えはもっと大きいですよ");
+      }
+    
+    else{
+      p1.textContent = ("正解です.おめでとう!");
+        console.log("正解です.おめでとう!");
+      }
+  }
+
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
--- practise-dom/dom-exercise.js
+++ practise-dom/dom-exercise.js
@@ -12,15 +12,51 @@ let data = [
 	{name:'沖縄', lat:26.2125, lng:127.68111}
 ];
 //// 注意: 以上は編集しないこと!
-
 // 練習4-2 メッセージ追加プログラム
-
-
+let h2 = document.querySelector('h2#ex42');
+let p = document.createElement('p');
+p.style='text-emphasis: sesame green';
+p.textContent = '写真表と都市の緯度経度のページです';
+h2.insertAdjacentElement('afterend', p);
 // 練習4-3 写真表作成プログラム
+let div1 = document.querySelector('div#phototable');
+let img1 = document.createElement('img');
+img1.setAttribute('src','./hanako.png');
+let p1 = document.createElement('p');
+p1.insertAdjacentElement('beforeend', img1);
+div1.insertAdjacentElement('beforeend', p1);
+
+let div2 = document.querySelector('div#phototable');
+let img2 = document.createElement('img');
+img2.setAttribute('src','./jiro.png');
+let p2 = document.createElement('p');
+p2.insertAdjacentElement('beforeend', img2);
+div2.insertAdjacentElement('beforeend', p2);
+
+let div3 = document.querySelector('div#phototable');
+let img3 = document.createElement('img');
+img3.setAttribute('src','./taro.png');
+let p3 = document.createElement('p');
+p3.insertAdjacentElement('beforeend', img3);
+div3.insertAdjacentElement('beforeend', p3);
+
+
 
 
 // 練習4-4 箇条書き削除プログラム
+let li = document.querySelector('ul#location');
+let li2 = document.querySelectorAll('li');
+for (let n of li2) {
+ n.remove();    
 
+  }
 
 // 練習4-5 箇条書き追加プログラム
-
+let li3 = document.querySelector('ul#location');
+for(m of data){
+ let text;
+ text = m.name + " 緯度:" + m.lat + ", 経度:" + m.lng;
+ let p3 = document.createElement("li");
+ p3.textContent = text;
+ li3.insertAdjacentElement('beforeend', p3);
+}
