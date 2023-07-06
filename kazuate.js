5052dea5f7e9507fc525746b14c323dd30762b8f
T2286752022 <228675@st.takushoku-u.ac.jp>
2023-07-06 17:06:04 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -3,7 +3,7 @@ let kotae = Math.floor(Math.random() * 10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 1;
+let kaisu = 0;
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
