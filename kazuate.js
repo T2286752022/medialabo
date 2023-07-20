e546477045ce29e35764529fe5b97bff52a28f66
T2286752022 <228675@st.takushoku-u.ac.jp>
2023-07-20 15:20:45 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -3,43 +3,42 @@ let kotae = Math.floor(Math.random() * 10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu = 1;
 
-// ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
-  let i = document.querySelector('input[name="yoso"]');
-  let yoso = parseInt(i.value);
+document.getElementById('print').addEventListener('click', function() {
 
-  kaisu += 1;
-  let k1 = document.querySelector('span#kaisu');
-  k1.textContent = kaisu;
-  let k2 = document.querySelector('span#answer');
-  k2.textContent = yoso;
+  // ボタンを押した後の処理をする関数 hantei() の定義
+  function hantei() {
+    let i = document.getElementById('yoso');
+    let yoso = parseInt(i.value);
 
-  let p1 = document.querySelector('p#result');
+    let k1 = document.querySelector('span#kaisu');
+    k1.textContent = kaisu;
+    let k2 = document.querySelector('span#answer');
+    k2.textContent = yoso;
 
-  if (kaisu >= 4) {
-    p1.textContent = "答えは" + kotae + "でした．すでにゲームは終わっています";
-    console.log("答えは" + kotae + "でした．すでにゲームは終わっています");
-  } else if (kaisu == 3 && kotae != yoso) {
-    p1.textContent = "まちがい.残念でした答えは" + kotae + "です．";
-    console.log("まちがい.残念でした答えは" + kotae + "です．");
-  } else if (kaisu == 3 && kotae == yoso) {
-    p1.textContent = "正解です.おめでとう!";
-    console.log("正解です.おめでとう!");
-  } else {
-    if (yoso > kotae) {
-      p1.textContent = "まちがい.答えはもっと小さいですよ";
-      console.log("まちがい.答えはもっと小さいですよ");
-    } else if (yoso < kotae) {
-      p1.textContent = "まちがい.答えはもっと大きいですよ";
-      console.log("まちがい.答えはもっと大きいですよ");
-    } else {
+    let p1 = document.querySelector('p#result');
+
+    if (kaisu >= 4) {
+      p1.textContent = "答えは" + kotae + "でした．すでにゲームは終わっています";
+      console.log("答えは" + kotae + "でした．すでにゲームは終わっています");
+    } else if (kaisu === 3 && kotae !== yoso) {
+      p1.textContent = "まちがい.残念でした答えは" + kotae + "です．";
+      console.log("まちがい.残念でした答えは" + kotae + "です．");
+    } else if (kotae === yoso) {
       p1.textContent = "正解です.おめでとう!";
       console.log("正解です.おめでとう!");
+    } else {
+      if (yoso > kotae) {
+        p1.textContent = "まちがい.答えはもっと小さいですよ";
+        console.log("まちがい.答えはもっと小さいですよ");
+      } else if (yoso < kotae) {
+        p1.textContent = "まちがい.答えはもっと大きいですよ";
+        console.log("まちがい.答えはもっと大きいですよ");
+      }
+      kaisu++;
     }
   }
-}
 
-let b = document.querySelector('#print');
-b.addEventListener('click', hantei);
+  hantei();
+});
