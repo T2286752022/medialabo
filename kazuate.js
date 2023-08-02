f7420d4c30235da0888556be015a6ff3d8165d5d
T2286752022 <228675@st.takushoku-u.ac.jp>
2023-08-03 07:59:51 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -1,44 +1,44 @@
-// 答え
-let kotae = Math.floor(Math.random() * 10) + 1;
-console.log('答え（デバッグ用）: ' + kotae);
+document.addEventListener('DOMContentLoaded', function() {
+  // 答え
+  let kotae = Math.floor(Math.random() * 10) + 1;
+  console.log('答え（デバッグ用）: ' + kotae);
 
-// 入力回数（予想回数）
-let kaisu = 1;
+  // 入力回数（予想回数）
+  let kaisu = 1;
 
-document.Youso('print').addEventListener('click', function() {
+  // 予想ボタンをクリックしたときの処理
+  document.getElementById('guessButton').addEventListener('click', function() {
+    // 予想した数字を取得
+    let guessInput = document.getElementById('guessInput');
+    let yoso = parseInt(guessInput.value);
 
-  // ボタンを押した後の処理をする関数 hantei() の定義
-  function hantei() {
-    let i = document.Youso('yoso');
-    let yoso = parseInt(i.value);
+    let SpanKaisu = document.querySelector('span#kaisu');
+    SpanKaisu.textContent = kaisu;
 
-    let k1 = document.querySelector('span#kaisu');
-    k1.textContent = kaisu;
-    let k2 = document.querySelector('span#answer');
-    k2.textContent = yoso;
+    let SpanAnswer = document.querySelector('span#answer');
+    SpanAnswer.textContent = yoso;
 
-    let p1 = document.querySelector('p#result');
+    let SpanResult = document.querySelector('p#result');
 
-    if (kaisu >= 4) {
-      p1.textContent = "答えは" + kotae + "でした．すでにゲームは終わっています";
-      console.log("答えは" + kotae + "でした．すでにゲームは終わっています");
-    } else if (kaisu === 3 && kotae !== yoso) {
-      p1.textContent = "まちがい.残念でした答えは" + kotae + "です．";
-      console.log("まちがい.残念でした答えは" + kotae + "です．");
-    } else if (kotae === yoso) {
-      p1.textContent = "正解です.おめでとう!";
-      console.log("正解です.おめでとう!");
-    } else {
-      if (yoso > kotae) {
-        p1.textContent = "まちがい.答えはもっと小さいですよ";
-        console.log("まちがい.答えはもっと小さいですよ");
-      } else if (yoso < kotae) {
-        p1.textContent = "まちがい.答えはもっと大きいですよ";
-        console.log("まちがい.答えはもっと大きいですよ");
+    if (kaisu < 3) {
+      if (yoso === kotae) {
+        SpanResult.textContent = 'せいかーい。おめでとう！';
+      } else {
+        if (yoso < kotae) {
+          SpanResult.textContent = 'ザンネーン！答えはもっと大きいよ。';
+        } else {
+          SpanResult.textContent = 'ザンネーン！答えはもっと小さいよ。';
+        }
       }
-      kaisu++;
+    } else if (kaisu === 3) {
+      if (yoso === kotae) {
+        SpanResult.textContent = 'せいかーい。おめでとうございます！';
+      } else {
+        SpanResult.textContent = 'ザンネーン！答えは' + kotae + 'でした〜';
+      }
+    } else if (kaisu > 3) {
+      SpanResult.textContent = 'もうゲームは終わってるよ。答えは' + kotae + 'でした〜';
     }
-  }
-
-  hantei();
+    kaisu = kaisu + 1;
+  });
 });
